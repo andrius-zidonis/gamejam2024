e61bb56225ccf92cbebe87c2631fd9593cc76e39
Andrius Zidonis <zidandrius@gmail.com>
2024-01-27 01:17:59 +0200

    add players, camera with follow and boundries

---
+++ Camera2D.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+@export var object1: Node
+@export var object2: Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.global_position = (object1.global_position + object2.global_position) * 0.5
+	
+	self.zoom = Vector2(0.5, 0.5)
---
+++ CharacterBody2D.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed = 400
+@export var player: int
+
+func get_input():
+	var input_direction = Input.get_vector("%s_left" % player, "%s_right" % player, "%s_up" % player, "%s_down" % player)
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
